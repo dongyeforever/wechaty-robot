ef5b5ea21bda127b712e4a033fd691c3cd2e7028
dongyeforever <dongye@lovek.vip>
2020-11-28 17:59:48 +0800

    修改群

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ export default {
     ALIAS: 'A独一无二的喵~',
     GROUP_LIST: [
         { topic: '测试' },
-        { id: '@@bc78c7aaab2cbec53c401bb1611b9e3333d6344b853aab85c8fa1a2d4a3cb328' }
+        { topic: '温馨之港' }
     ],
     WEATHER_JOB: '0 0 23 * * *', // 6个占位符：秒、分、时、日、月、周几
     HOUSE_JOB: '0 0 18 * * *',
