510b3ed4be51f714097983c7b6b0df1324d5ba8e
dongyeforever <dongye@lovek.vip>
2021-02-05 12:32:32 +0800

    增加日期判断

--- src/filter/new-year-filter.ts
+++ src/filter/new-year-filter.ts
@@ -6,12 +6,24 @@ export default class NewYearFilter implements IFilter {
         // 对新年祝福的处理
         //  新年 新春 春节 + 快乐
         const text = message.text()
+        // 如果不是春节附近 return false
+        if(!(this.isToday("2021-1-11") || this.isToday("2021-1-12"))) {
+            return false
+        }
         if (!text.startsWith("#新年快乐") && this.contains(text, "快乐") &&
             (this.contains(text, "新年") || this.contains(text, "新春") || this.contains(text, "春节"))) {
             this.sayMessage(message, "#新年快乐")
             return true
         }
         return false
+    }
+
+    isToday(str: string){
+        const date = new Date();
+        var y = date.getFullYear(); // 年
+        var m = date.getMonth() + 1; // 月份从0开始的
+        var d = date.getDate(); //日
+        return str == (y + '-' + m + '-' + d);
     }
 
     contains(text: string, keywords: string): boolean {
